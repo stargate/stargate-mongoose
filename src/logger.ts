3e5214f64528cc9b792363443993b0d38e539c45
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2023-05-01 12:31:11 -0400

    Removed unused function

--- src/logger.ts
+++ src/logger.ts
@@ -25,6 +25,3 @@ export const logger = winston.createLogger({
 export const setLevel = (level: string) => {
   consoleTransport.level = level;
 };
-export const getLevel = () => {
-  return consoleTransport.level;
-};
