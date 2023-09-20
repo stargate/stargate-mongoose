23013c3f6c78398af451e59b0cbe8400b0ae82d3
Kathiresan Selvaraj <96088452+kathirsvn@users.noreply.github.com>
2023-09-20 11:02:50 -0400

    Added test for similarity score (#130)
    * Added test for similarity score
    
    * lint fix and also fixed npm run build
    
    * findOne projection fix
    
    * lint fix

--- src/collections/options.ts
+++ src/collections/options.ts
@@ -50,6 +50,7 @@ export const findInternalOptionsKeys: Set<string> = new Set(
  */
 export interface FindOneOptions {
     sort?: Record<string, 1 | -1>;
+    projection?: ProjectionOption;
 }
 
 /**
