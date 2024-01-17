baf6b237e3f061dd41248d20f4b441f6ea3d0b96
Valeri Karpov <val@karpov.io>
2024-01-17 06:30:00 -0500

    add `listCollections()` function to driver as wrapper around `findCollections()` (#176)

--- tests/driver/index.test.ts
+++ tests/driver/index.test.ts
@@ -355,6 +355,17 @@ describe('Driver based tests', async () => {
             );
         });
 
+        it('handles listCollections()', async () => {
+            const personSchema = new mongooseInstance.Schema({
+                name: { type: String, required: true }
+            });
+            const Person = mongooseInstance.model('Person', personSchema);
+            await Person.init();
+            await Person.deleteMany({});
+            const collections = await mongooseInstance.connection.listCollections();
+            assert.ok(collections.includes('people'), collections);
+        });
+
         async function createMongooseInstance() {
             const mongooseInstance = new mongoose.Mongoose();
             mongooseInstance.setDriver(StargateMongooseDriver);
