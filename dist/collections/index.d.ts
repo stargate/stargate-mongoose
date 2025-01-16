d3b6134ad34ba984e6bddb5ac28e32d265d2681b
Valeri Karpov <val@karpov.io>
2025-01-16 12:59:13 -0500

    remove dist again

--- dist/collections/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Client } from './client';
-export { Collection } from './collection';
-export { FindOneAndDeleteOptions, FindOneAndReplaceOptions, FindOneAndUpdateOptions, FindOneOptions, FindOptions, IndexingOptions, InsertManyOptions, UpdateManyOptions, UpdateOneOptions, DeleteOneOptions, VectorOptions } from './options';
-export { Client as MongoClient } from './client';
-export { createAstraUri, createStargateUri } from './utils';
--- dist/tableDefinitionFromSchema.d.ts
+++
@@ -1,3 +0,0 @@
-import { CreateTableDefinition } from '@datastax/astra-db-ts';
-import { Schema } from 'mongoose';
-export default function tableDefinitionFromSchema(schema: Schema): CreateTableDefinition;
