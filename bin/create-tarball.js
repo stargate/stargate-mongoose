2355bb1310cf2d383f7a45f30e1fef10f373aad6
Valeri Karpov <val@karpov.io>
2025-05-05 18:01:48 -0400

    fix e2e tests

--- bin/create-tarball.js
+++ bin/create-tarball.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { execSync } = require('child_process');
+const fs = require('fs');
+
 // hard-code name because `npm pack` transforms namespaced names
 const name = 'datastax-astra-mongoose';
 const { version } = JSON.parse(fs.readFileSync('./package.json'));
