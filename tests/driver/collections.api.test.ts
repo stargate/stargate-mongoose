004b5f3db3d74022c4b569b3458ca0af42e8eeac
Valeri Karpov <val@karpov.io>
2026-06-19 19:38:17 -0400

    basic useDb() implementation

--- tests/driver/collections.api.test.ts
+++ tests/driver/collections.api.test.ts
@@ -854,6 +854,38 @@ describe('COLLECTIONS: mongoose Model API level tests with collections', async (
             await connection.openUri(testClient!.uri, testClient!.options);
             assert.ok((await promise.then(res => res.map(obj => obj.name))).includes(Product.collection.collectionName));
         });
+        it('API ops tests useDb()', async function() {
+            const connection = mongooseInstance.createConnection(testClient!.uri, testClient!.options) as unknown as AstraMongooseDriver.Connection;
+            await connection.asPromise();
+            const { keyspaceName } = parseUri(testClient!.uri);
+            const childConnection = connection.useDb(keyspaceName, { useCache: true });
+            assert.strictEqual(connection.useDb(keyspaceName, { useCache: true }), childConnection);
+
+            const promise = childConnection.listCollections({ nameOnly: false });
+
+            assert.ok((await promise.then(res => res.map(obj => obj.name))).includes(Product.collection.collectionName));
+
+            await connection.close();
+        });
+        it('API ops tests useDb() before openUri()', async function() {
+            const connection = mongooseInstance.createConnection() as unknown as AstraMongooseDriver.Connection;
+            const { keyspaceName } = parseUri(testClient!.uri);
+            const childConnection = connection.useDb(keyspaceName, { useCache: true });
+            assert.strictEqual(connection.useDb(keyspaceName, { useCache: true }), childConnection);
+
+            const openPromise = once(childConnection, 'open');
+            const promise = childConnection.listCollections({ nameOnly: false });
+
+            await connection.openUri(testClient!.uri, testClient!.options);
+            await openPromise;
+
+            assert.strictEqual(childConnection.client, connection.client);
+            assert.notStrictEqual(childConnection.db, connection.db);
+            assert.strictEqual(childConnection.keyspaceName, keyspaceName);
+            assert.ok((await promise.then(res => res.map(obj => obj.name))).includes(Product.collection.collectionName));
+
+            await connection.close();
+        });
         it('API ops tests createConnection() with no buffering', async function() {
             const connection = mongooseInstance.createConnection(testClient!.uri, { ...testClient!.options, bufferCommands: false }) as unknown as AstraMongooseDriver.Connection;
             await connection.asPromise();
