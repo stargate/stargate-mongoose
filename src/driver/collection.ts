d5d338a87b13a0701fa3bd12ce61bb6248014823
Valeri Karpov <val@karpov.io>
2026-08-30 21:31:47 -0400

    remove unnecessary Promise<any> on findOneAndUpdate

--- src/driver/collection.ts
+++ src/driver/collection.ts
@@ -264,10 +264,28 @@ export class Collection<DocType extends Record<string, unknown> = Record<string,
      */
 
     async findOneAndUpdate(
+      filter: Filter,
+      update: CollectionUpdateFilter<DocType> | TableUpdateFilter<DocType> | Record<string, unknown>[],
+      options: (FindOneAndUpdateOptions & { includeResultMetadata: true })
+    ): Promise<{ value: WithId<DocType> | null, ok: 1 }>;
+
+    async findOneAndUpdate(
+      filter: Filter,
+      update: CollectionUpdateFilter<DocType> | TableUpdateFilter<DocType> | Record<string, unknown>[],
+      options: (FindOneAndUpdateOptions & { includeResultMetadata: false })
+    ): Promise<WithId<DocType> | null>;
+
+    async findOneAndUpdate(
+      filter: Filter,
+      update: CollectionUpdateFilter<DocType> | TableUpdateFilter<DocType> | Record<string, unknown>[],
+      options?: FindOneAndUpdateOptions
+    ): Promise<WithId<DocType> | null>;
+
+    async findOneAndUpdate(
         filter: Filter,
         update: CollectionUpdateFilter<DocType> | TableUpdateFilter<DocType> | Record<string, unknown>[],
         options?: FindOneAndUpdateOptions
-    ): Promise<any> {
+    ) {
         options = options ?? {};
         if (Array.isArray(update)) {
             throw new AstraMongooseError('Astra-mongoose does not support update pipelines', { update });
@@ -288,9 +306,9 @@ export class Collection<DocType extends Record<string, unknown> = Record<string,
 
         return await this.collection.findOneAndUpdate(filter, update, requestOptions).then((value: Record<string, unknown> | null) => {
             if (options.includeResultMetadata) {
-                return { value: deserializeDoc<DocType>(value) };
+                return { value: deserializeDoc<WithId<DocType>>(value), ok: 1 };
             }
-            return deserializeDoc<DocType>(value);
+            return deserializeDoc<WithId<DocType>>(value);
         });
     }
 
